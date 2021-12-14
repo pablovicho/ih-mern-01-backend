6dd5ee48876786265ef56a94a6acb4ceca321bcf
pablo valdés <pablovvb@gmail.com>
2021-12-14 17:50:22 -0600

    permisos a terceros en index.js

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 // 1. IMPORTACIONES
 const express       = require("express")
 const app           = express()
+const cors          = require("cors")
 require("dotenv").config()
 const connectDB = require("./config/db")
 
 // 2. MIDDLEWARES
 //todas las peticiones y respuestas se manejan en protocolo json
 connectDB()
+
+//habilitar cors: accesos de ambientes de desarrollo de terceros
+app.use(cors())
 app.use(express.json())
 
 
